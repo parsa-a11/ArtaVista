eedd5e1af9942b806ff56cfbc70b3fcb9c758aff
parsa-a11 <atmadyparsa@gmail.com>
2026-02-12 12:12:02 +0330

    Create ArtaVista project

---
+++ src/main/java/com/example/artavista/ArtaVistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.artavista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtaVistaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtaVistaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/artavista/ArtaVistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artavista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtaVistaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
